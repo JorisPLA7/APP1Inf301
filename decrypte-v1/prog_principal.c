d4f075ce90d741a82acb03328ddb57fcaf27a43a
Manon <33965049+hackerdu38@users.noreply.github.com>
2018-09-30 20:40:47 +0200

    Update prog_principal.c

--- decrypte-v1/prog_principal.c
+++ decrypte-v1/prog_principal.c
@@ -7,22 +7,22 @@
 ---DEBUG--- crcr   .....  ALICE : MESSAGE DE BOB RETROUVE MOI SUR 'planB' LE MOT DE PASSE EST 'qui vivra verra' LAISSE MOI UN MESS...crcrcrc zbmbbzzzzz bz b zbbb
 */
 
-char decalage(char lettreLue, int decal)
-{
+char decalage(char lettreLue, int decal){
+    //Pour decoder le message, on va decaler les lettres en se deplacant du nombre de places indique par "decal" dans la table ASCII
     char lettreSuivante;
-    
+    //On doit faire attention a ne pas depasser de l'alphabet dans lequel on est
     if ('a' <= lettreLue && lettreLue <= 'z' ) {
         lettreSuivante = lettreLue + decal;
         if (lettreSuivante > 'z'){
-            lettreSuivante =  lettreSuivante-26;
+            lettreSuivante =  lettreSuivante-26; //Si l'on arrive a la fin de l'alphabet, on repasse au debut
         }
         if (lettreSuivante < 'a'){
-            lettreSuivante =  lettreSuivante+26;
+            lettreSuivante =  lettreSuivante+26; //Si l'on arrive au debut de l'alphabet, on repasse a la fin
         }
 
     }
 
-    else if ('A' <= lettreLue && lettreLue <= 'Z' ) {
+    else if ('A' <= lettreLue && lettreLue <= 'Z' ) { //Meme chose pour les lettres majuscules
         lettreSuivante = lettreLue + decal;
         if (lettreSuivante > 'Z'){
             lettreSuivante = lettreSuivante-26;
@@ -66,7 +66,7 @@ int main() {
     // ici on passe tout en majuscules.
     for (int j=0; j<5; j++) {
         for (int i =0; reponse[i]!=0; i++)
-            reponse[i] = decalage(reponse[i], -5);
+            reponse[i] = decalage(reponse[i], -5); //met dans reponse[i] la lettre decodee grace a la fonction decalage
         envoyer_recevoir(reponse, reponse);
     }
     
